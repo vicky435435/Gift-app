95845ec5dc5432fb50210cf644b595792d5d7590
Ramkrushna435 <vickymaneshwar435@gmail.com>
2024-06-16 18:45:23 +0530

    code

---
+++ gift-shop/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'gift-shop',
+  webDir: 'www'
+};
+
+export default config;
